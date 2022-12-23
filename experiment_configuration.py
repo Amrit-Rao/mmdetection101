e79205d759fa1f2e0277c3a3279af5e66a89a47d
Amrit-Rao <superamritrao@rediffmail.com>
2022-12-23 18:57:30 +0530

    Updated experiment_manager.py

--- experiment_configuration.py
+++ experiment_configuration.py
@@ -16,16 +16,14 @@ classes = (
 )
 type = "CocoDataset"
 
-a = ExperimentManager(name=str(current_datetime))
-a.getConfigFromPath(
+experiment = ExperimentManager(name=str(current_datetime))
+experiment.getConfigFromPath(
     config_path="mmdetection/configs/faster_rcnn/faster_rcnn_x101_32x4d_fpn_1x_coco.py"
 )
-a.buildDataset(data_path="data/kitti-coco", classes=classes, type=type)
-a.buildModel(
+experiment.buildDataset(data_path="data/kitti-coco", classes=classes, type=type)
+experiment.buildModel(
     classes=classes, checkpoint_path="train_runs/2022-12-20 11:10:03.910932/latest.pth"
 )
-# a.train(output_path="train_runs", num_of_epochs=4)
-# a.evaluate(output_path="eval_runs")
-a.predict(output_path="predictions")
-
-# out: results.bbox.json and results.segm.json
+# experiment.train(output_path="train_runs", num_of_epochs=4)
+# experiment.evaluate(output_path="eval_runs")
+experiment.predict(output_path="predictions")
