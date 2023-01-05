4756305acd9b5961da9ec47b2e525f533e46cb42
Amrit-Rao <superamritrao@rediffmail.com>
2023-01-05 21:40:05 +0530

    Updated visualization.py

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -1,7 +1,7 @@
 from segmentation import Visualize
 from segmentation import ExperimentManager
 
-json_file_path = "data/kitti-coco/validation/labels.json"
+dir_path = "data/kitti-coco/validation"
 classes = (
     "Car",
     "Cyclist",
@@ -18,16 +18,19 @@ type = "CocoDataset"
 
 experiment = ExperimentManager()
 experiment.getConfigFromPath(
-    config_path="mmdetection/configs/faster_rcnn/faster_rcnn_x101_32x4d_fpn_1x_coco.py"
+    config_path="mmdetection/configs/faster_rcnn/faster_rcnn_x101_32x4d_fpn_1x_coco.py",
 )
 experiment.buildDataset(data_path="data/kitti-coco", classes=classes, type=type)
 experiment.buildModel(
-    classes=classes, checkpoint_path="train_runs/2022-12-20 11:10:03.910932/latest.pth"
+    classes=classes,
+    checkpoint_path="train_runs/2022-12-20 11:10:03.910932/latest.pth",
 )
 model = experiment.getModel()
 val_dataloader = experiment.getValDataloader()
 
 visualization = Visualize()
 visualization.scatterPlot(
-    val_json_path=json_file_path, val_dataloader=val_dataloader, model=model
+    dir_path=dir_path,
+    val_dataloader=val_dataloader,
+    model=model,
 )
